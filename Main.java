69b1d741ad5ef598fa15dd2e6d8afe87357f0b86
Palash Khandelwal <80159219+PalashKhandelwal@users.noreply.github.com>
2024-02-03 18:24:29 +0530

    InputMainCompletion

--- Main.java
+++ Main.java
@@ -462,6 +462,98 @@ class Write implements Runnable{
     }
 }
 public class Main {
+    public static void main(String[] args) {
+        
+        try{
+            File f = new File("Backup");
+            if(f.exists()){
+                FileInputStream fin = new FileInputStream(f);
+                ObjectInputStream ois = new ObjectInputStream(fin);
+                Hotel.hotel_ob = (holder)ois.readObject();
+            }
+            Scanner sc = new Scanner(System.in);
+            int ch, ch2;
+            char wish;
+            exitCode:
+            do{
+                System.out.println(" \n1.Display room details\n2.Display rooms available\n3.Book Room\n4.Order Food\n5.Checkout\n6.Exit\nEnter your Choice : ");
+                ch = sc.nextInt();
+                switch (ch) {
 
-    
+                    case 1:
+                        System.out.println("\nChoose room type : \n1.Luxury Double Room \n2.Deluxe Double Room \n3.Luxury Single Room \n4.Deluxe Single Room\n");
+                        ch2 = sc.nextInt();
+                        Hotel.Features(ch2);
+                        break;
+                
+                    case 2:
+                        System.out.println("\nChoose room type :\n1.Luxury Double Room \n2.Deluxe Double Room \n3.Luxury Single Room\n4.Deluxe Single Room\n");
+                        ch2 = sc.nextInt();
+                        Hotel.Availability(ch2);
+                        break;
+
+                    case 3:
+                        System.out.println("\nChoose room type :\n1.Luxury Double Room \n2.Deluxe Double Room \n3.Luxury Single Room\n4.Deluxe Single Room\n");
+                        ch2 = sc.nextInt();
+                        Hotel.bookRoom(ch2);                     
+                        break;
+                    
+                    case 4 : 
+                        System.out.println("\nRoom Number - ");
+                        ch2 = sc.nextInt();
+
+                        if(ch2>60)
+                            System.out.println("\nRoom does not exist!");
+
+                        else if(ch2>40)
+                            Hotel.Order(ch2-41, 4);
+
+                        else if(ch2>30)
+                            Hotel.Order(ch2-31,3);
+
+                        else if(ch2>10)
+                            Hotel.Order(ch2-11,2);
+
+                        else if(ch2>0)
+                            Hotel.Order(ch2-1,1);
+
+                        else
+                            System.out.println("Room doesn't exist");
+                        break;
+                    
+                    case 5 : 
+                        System.out.println("\nRoom Number -");
+                        ch2 = sc.nextInt();
+                        if(ch2>60)
+                         System.out.println("Room doesn't exist");
+                        else if(ch2>40)
+                            Hotel.DeallocateRooms(ch2-41,4);
+                        else if(ch2>30)
+                            Hotel.DeallocateRooms(ch2-31,3);
+                        else if(ch2>10)
+                            Hotel.DeallocateRooms(ch2-11,2);
+                        else if(ch2>0)
+                            Hotel.DeallocateRooms(ch2-1,1);
+                        else
+                            System.out.println("Room doesn't exist");
+                         break;
+                    case 6 : break exitCode;
+                }
+
+                System.out.println("\nContinue : (y/n)");
+                wish = sc.next().charAt(0);
+                if(!(wish == 'y' || wish == 'Y' || wish == 'n' || wish == 'N')){
+                    System.out.println("Invalid Option");
+                    System.out.println("\nContinue : (y/n)");
+                    wish=sc.next().charAt(0);
+                }
+            }while(wish=='y'||wish=='Y');    
+        
+                Thread t=new Thread(new Write(Hotel.hotel_ob));
+                t.start();
+            }
+            catch(Exception e){
+                System.out.println("\nInvalid Input");
+            }
+    }   
 }
