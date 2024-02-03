09bb05ea2d60276a73a54cf0347d5d8a06f44a38
Palash Khandelwal <80159219+PalashKhandelwal@users.noreply.github.com>
2024-02-03 17:48:47 +0530

    OrderFunction(FOOD)

--- Main.java
+++ Main.java
@@ -411,6 +411,55 @@ class Hotel{
                 break;
         }
     }
+
+    static void Order(int rn, int rtype){
+        int i, q;
+        char wish; 
+        try{
+            System.out.println("\n==========\n    Menu : \n\n1. Sandwich\tRs.100\n2.Pizza\tRs.180\n3.Coke\tRs.50\n4.Brownie with Ice Cream\tRs.150");
+            do{
+                i = sc.nextInt();
+                System.out.println("\nQuantity : ");
+                q = sc.nextInt();
+                switch(rtype){
+                    case 1 : hotel_ob.luxury_doubleroom[rn].food.add(new Food(i, q));
+                        break;
+                    case 2 : hotel_ob.deluxe_doubleroom[rn].food.add(new Food(i, q));
+                        break;
+                    case 3 : hotel_ob.luxury_singleroom[rn].food.add(new Food(i, q));
+                        break;
+                    case 4 : hotel_ob.deluxe_singleroom[rn].food.add(new Food(i, q)); 
+                        break;
+                }
+                    System.out.println("\nDo you want to order anything else ? (y/n)");
+                    wish = sc.next().charAt(0);
+            }while(wish == 'y' || wish == 'Y');
+        }
+        catch (NullPointerException e){
+            System.out.println("\nRoom not Booked");
+        }
+        catch(Exception e){
+            System.out.println("\nCannot be done");
+        }
+    }
+}
+
+class Write implements Runnable{
+    holder hotel_ob;
+    Write(holder hotel_ob){
+        this.hotel_ob = hotel_ob;
+    }
+    @Override
+    public void run(){
+        try{
+            FileOutputStream fout = new FileOutputStream("Backup");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(hotel_ob);
+        }
+        catch(Exception e){
+            System.out.println("\nError in writing" +e);
+        }
+    }
 }
 public class Main {
 
